51b2ce3265991c52170c2ce20039edb389d24d1f
Диана <heilig1996@mail.ru>
2025-05-17 17:43:28 +0300

    add app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates')
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
