0293a404f7157c47ac50e62a68dbcb6249b8dee4
Диана <heilig1996@mail.ru>
2025-05-26 20:31:50 +0300

    add database.sql

--- database.sql
+++ database.sql
@@ -3,3 +3,14 @@ CREATE TABLE urls IF NOT EXISTS (
     name VARCHAR(255) NOT NULL UNIQUE,
     created_at TIMESTAMP
 );
+
+
+CREATE TABLE url_checks IF NOT EXISTS (
+    id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id INT REFERENCES urls(id),
+    status_code INT,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description TEXT,
+    created_at TIMESTAMP
+);
