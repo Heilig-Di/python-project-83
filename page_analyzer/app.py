5f7a9d12e6dfb686e604770e5ae4d4bb57dc2969
Диана <heilig1996@mail.ru>
2025-05-28 17:43:22 +0300

    add .

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -7,14 +7,14 @@ from dotenv import load_dotenv
 from urllib.parse import urlparse
 
 load_dotenv()
-app = Flask(__name__, template_folder='../templates')
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app = Flask(__name__, template_folder="../templates")
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 def get_db_connection():
-    return psycopg2.connect(DATABASE_URL, sslmode='require')
+    return psycopg2.connect(DATABASE_URL, sslmode="require")
 
 
 @app.route("/")
@@ -22,81 +22,86 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/urls', methods=['POST'])
+@app.route("/urls", methods=["POST"])
 def add_url():
-    url = request.form.get('url')
+    url = request.form.get("url")
 
     if not validators.url(url):
-        flash('Некорректный URL', 'danger')
+        flash("Некорректный URL", "danger")
         return render_template("index.html"), 422
 
     if len(url) > 255:
-        flash('Некорректный URL', 'danger')
+        flash("Некорректный URL", "danger")
         return render_template("index.html"), 422
 
-    normalized_url = f'{urlparse(url).scheme}://{urlparse(url).netloc}'
+    normalized_url = f"{urlparse(url).scheme}://{urlparse(url).netloc}"
     with get_db_connection() as conn:
         with conn.cursor() as cur:
-            cur.execute(
-                'SELECT id from urls WHERE name = %s;',
-                (normalized_url,)
-            )
-        flash('Страница уже существует', 'success')
+            cur.execute("SELECT id from urls WHERE name = %s;", (normalized_url,))
+        flash("Страница уже существует", "success")
 
         cur.execute(
-            'INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id;',
-            (normalized_url, datetime.now())
+            "INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id;",
+            (normalized_url, datetime.now()),
         )
         url_id = cur.fetchone()[0]
         conn.commit()
-        flash('Страница успешно добавлена', 'success')
+        flash("Страница успешно добавлена", "success")
 
-    return redirect(url_for('show_url', id=url_id))
+    return redirect(url_for("show_url", id=url_id))
 
 
-@app.route('/urls')
+@app.route("/urls")
 def list_urls():
     with get_db_connection() as conn:
         with conn.cursor() as cur:
-            cur.execute('SELECT * FROM urls ORDER BY created_at DESC;')
+            cur.execute('''
+                        SELECT
+                        u.id, u.name, uc.created_at
+                        FROM urls as u
+                        JOIN urls_check as uc ON u.id=uc.url_id
+                        GROUP BY u.id, u.name, uc.created_at
+                        ORDER BY u.id DESC''')
 
         urls = cur.fetchall()
-    return render_template('urls/index.html', urls=urls)
+    return render_template("urls/index.html", urls=urls)
 
 
-@app.route('/urls/<int:id>')
+@app.route("/urls/<int:id>")
 def show_url(id):
     with get_db_connection() as conn:
         with conn.cursor() as cur:
-            cur.execute('SELECT * FROM urls WHERE id = %s;', (id,))
+            cur.execute("SELECT * FROM urls WHERE id = %s;", (id,))
             url = cur.fetchone()
 
-            cur.execute('''
+            cur.execute("""
                         SELECT url_id, created_at
                         FROM url_ckecks
                         WHERE url_id = %s;
-                        ''', (id,))
+                        """, (id,))
             checks = cur.fetchone()
-    return render_template('urls/show.html', url=url, checks=checks)
+    return render_template("urls/show.html", url=url, checks=checks)
 
 
-@app.post('urls/<int:id>/checks')
+@app.post("urls/<int:id>/checks")
 def check_id(id):
     try:
         with get_db_connection() as conn:
             with conn.cursor() as cur:
-                cur.execute('SELECT * FROM urls WHERE id = %s;', (id,))
+                cur.execute("SELECT * FROM urls WHERE id = %s;", (id,))
 
-                cur.execute('''
+                cur.execute(
+                    """
                             INSERT INTO url_checks (url_id)
                             VALUES (%s)
                             RETURNING id;
-                            ''', (id,))
+                            """,
+                    (id,),
+                )
             conn.commit()
-        flash('Страница успешно проверена', 'success')
-
+        flash("Страница успешно проверена", "success")
 
     except Exception as e:
 
-        flash('Произошла ошибка при проверке', 'danger')
-        return redirect(url_for('show_url', id=id))
+        flash("Произошла ошибка при проверке", "danger")
+        return redirect(url_for("show_url", id=id))
