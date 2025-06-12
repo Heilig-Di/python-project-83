39a7a009bfce53a3ac4c8a1109cf22466971fa0b
Диана <heilig1996@mail.ru>
2025-06-12 19:33:55 +0300

    add .

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, flash, redirect, url_for
 import os
 import psycopg2
 import validators
+import requests
 from datetime import datetime
 from dotenv import load_dotenv
 from urllib.parse import urlparse
@@ -57,11 +58,11 @@ def list_urls():
         with conn.cursor() as cur:
             cur.execute('''
                         SELECT
-                        u.id, u.name, uc.created_at
+                        u.id, u.name, MAX(uc.created_at), uc.status_code
                         FROM urls as u
-                        JOIN urls_check as uc ON u.id=uc.url_id
-                        GROUP BY u.id, u.name, uc.created_at
-                        ORDER BY u.id DESC''')
+                        JOIN url_checks as uc ON u.id=uc.url_id
+                        GROUP BY u.id, u.name
+                        ORDER BY u.id DESC;''')
 
         urls = cur.fetchall()
     return render_template("urls/index.html", urls=urls)
@@ -75,8 +76,8 @@ def show_url(id):
             url = cur.fetchone()
 
             cur.execute("""
-                        SELECT url_id, created_at
-                        FROM url_ckecks
+                        SELECT url_id, created_at, status_code
+                        FROM url_checks
                         WHERE url_id = %s;
                         """, (id,))
             checks = cur.fetchone()
@@ -105,3 +106,34 @@ def check_id(id):
 
         flash("Произошла ошибка при проверке", "danger")
         return redirect(url_for("show_url", id=id))
+
+
+@app.post("/urls/<int:id>/checks")
+def check_url(id):
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT name FROM urls WHERE id = %s;", (id,))
+            url_record = cur.fetchone()
+
+            if not url_record:
+                flash("Страница не найдена", "danger")
+                return redirect(url_for('list_urls'))
+
+            url = url_record[0]
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError:
+        flash('Произошла ошибка при проверке', 'danger')
+        return redirect(url_for('show_url', id=id))
+
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("""
+                        INSERT INTO url_checks (url_id, status_code)
+                        VALUES (%s, %s);
+                        """, (id, status_code))
+            conn.commit()
+    flash('Страница успешно проверена', 'success')
+    return redirect(url_for("show_url", id=id))
