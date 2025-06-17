20f28aa6c6d09ae120cd0349f5332e17132bc392
Диана <heilig1996@mail.ru>
2025-06-17 19:05:28 +0300

    add .

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -47,7 +47,7 @@ def add_url():
                 return redirect(url_for("show_url", id=url_record[0]))
             else:
                 cur.execute(
-                    "INSERT INTO urls (name) VALUES (%s) RETURNING id, created_at;",
+                    "INSERT INTO urls (name, created_at) VALUES (%s, CURRENT_DATE) RETURNING id, created_at;",
                     (normalized_url,),
                 )
                 new_record = cur.fetchone()
